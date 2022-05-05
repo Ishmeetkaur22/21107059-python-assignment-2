f75925f003c685d61290f637b40a119259c4d134
Ishmeetkaur22 <104060606+Ishmeetkaur22@users.noreply.github.com>
2022-05-05 14:28:15 +0530

    Add files via upload
    ISHMEET KAUR
    21107059
    MECHANICAL
    PYTHON ASSIGNMENT 2

---
+++ Question 2.py
@@ -0,0 +1,7 @@
+name = "ISHMEET"
+SID = "21107059"
+CGPA = "9.9"
+department = "MECHANICAL"
+
+
+print("Hey,",name, "Here!\nMy SID is", SID,"\nI am from", department, "department and my CGPA is", CGPA )
